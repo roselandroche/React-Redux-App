b837c59ea335c0376159cc149f718989af4f5eba
roselandroche <r.landroche@gmail.com>
2019-10-24 21:16:18 -0400

    Set up action and reducer file for axios call

---
+++ react-redux-app/src/actions/quote.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const GET_QUOTE_START = 'GET_QUOTE_START';
+export const GET_QUOTE_SUCCESS = 'GET_QUOTE_SUCCESS';
+export const GET_QUOTE_FAILURE = 'GET_QUOTE_FAILURE';
+
+export function getQuote() {
+    return dispatch => {
+        dispatch({ type: GET_QUOTE_START});
+
+        axios  
+            .get('https://quote-garden.herokuapp.com/quotes/random')
+            .then(res => {
+                dispatch({ type: GET_QUOTE_SUCCESS, payload: res.data.quoteText})
+            })
+            .catch(err => {
+                dispatch({ type: GET_QUOTE_FAILURE, payload: err})
+            })
+    }
+}
--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { logger } from 'redux-logger'
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import { reducer } from './reducers/feeling';
 
-const store = createStore(reducer, applyMiddleware(logger))
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
     <Provider store={store}>
